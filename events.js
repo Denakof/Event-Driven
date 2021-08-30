1518e1945185cffcaf5d193b1767c8ca3fdb2b81
denakof <denakofahi_21@yahoo.com>
2021-08-31 02:45:52 +0300

    lab11

---
+++ events.js
@@ -0,0 +1,7 @@
+'use stirct'
+const Events = require('events');
+const events = new Events();
+
+
+
+module.exports=events;
---
+++ src/vendor.js
@@ -0,0 +1,23 @@
+"use strict";
+require("dotenv").config();
+const events = require("../events");
+const faker = require("faker");
+
+setInterval(() => {
+  const Order = {
+    store: process.env.STORE || "NINJA",
+    orderID: faker.datatype.uuid(),
+    customer: faker.name.findName(),
+    address: faker.address.streetName(),
+  };
+  events.emit("pickup", Order);
+  console.log(Order);
+}, 5000);
+
+
+
+events.on("delivered", (payload) => {
+  console.log(`VENDOR: Thank you for delivering ${payload.orderID}`);
+});
+
+module.exports=events
